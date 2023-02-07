8a42bbd6d39291d21032acbbcfa760fac893960d
jabirdev <cs.jabirdev@gmail.com>
2023-02-07 13:29:42 +0700

    add pagination to playlist response

--- routes/yt/playlist/index.js
+++ routes/yt/playlist/index.js
@@ -1,13 +1,17 @@
 const yt = require('../lib')
+const paginator = require('../other/paginator')
 
 const channelId = async (req, res) => {
+    const page = req.query.page || 1
+    const per_page = req.query.per_page || 10
     const channelId = req.params.channelId
     const response = await yt.getPlaylist(channelId)
     if (response) {
+        const paginedPlaylist = paginator(response.items, page, per_page)
         res.json({
             error: false,
             message: 'Playlist fetched successfully',
-            data: response.items,
+            data: paginedPlaylist,
             continuation: response.continuation
         })
     } else {
